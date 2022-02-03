300607e48b6b3853090888748c7eec02e5ec170c
OnlyDropped <asheburenkov@yandex.ru>
2022-02-03 16:58:10 +0300

    added changes index.html?

--- js/app.js
+++ js/app.js
@@ -2,4 +2,7 @@ $('.slider-wrap').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
   centerMode: true,
+  arrows: true,
+  nextArrow: '<button type="button" class="slick-next"><img src="img/slider/next.png" alt=""></button>',
+  prevArrow: '<button type="button" class="slick-prev"><img src="img/slider/prev.png" alt=""></button>',
 });
